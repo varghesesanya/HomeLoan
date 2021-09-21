4b28b902ff61717db10b7252e7091d9427c7aa94
mandar <mandarjadhav139@gmail.com>
2021-09-21 16:29:16 +0530

    models added

---
+++ src/main/java/com/barclays/homeloans/model/Loan.java
@@ -0,0 +1,169 @@
+package com.barclays.homeloans.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+public class Loan {
+	
+	@Column
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanId;
+	
+	
+	@Column
+	@NotNull(message = "Total Loan Amount cannot be Null")
+	private Long totalLoanAmount;
+	
+	
+	@Column
+	@NotNull(message = "Interest Rate cannot be Null")
+	private double interestRate;
+	
+	@Column
+	@NotNull(message = "Tenure cannot be Null")
+	private Long tenure;
+	
+	@Column
+	@NotNull(message = "Status cannot be Null")
+	private String status;
+	
+	@Lob
+	@Type(type = "org.hibernate.type.ImageType")
+	@Column
+    private byte[] propertyImage;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	private SavingsAccount savingsAccount;
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name="loanId")
+	private List<LoanRepayment> loanRepayment;
+
+	
+	public Loan() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Loan(@NotNull(message = "Total Loan Amount cannot be Null") Long totalLoanAmount,
+			@NotNull(message = "Interest Rate cannot be Null") double interestRate,
+			@NotNull(message = "Tenure cannot be Null") Long tenure,
+			@NotNull(message = "Status cannot be Null") String status, byte[] propertyImage,
+			SavingsAccount savingsAccount, List<LoanRepayment> loanRepayment) {
+		super();
+		this.totalLoanAmount = totalLoanAmount;
+		this.interestRate = interestRate;
+		this.tenure = tenure;
+		this.status = status;
+		this.propertyImage = propertyImage;
+		this.savingsAccount = savingsAccount;
+		this.loanRepayment = loanRepayment;
+	}
+
+
+	public Long getLoanId() {
+		return loanId;
+	}
+
+
+	public void setLoanId(Long loanId) {
+		this.loanId = loanId;
+	}
+
+
+	public Long getTotalLoanAmount() {
+		return totalLoanAmount;
+	}
+
+
+	public void setTotalLoanAmount(Long totalLoanAmount) {
+		this.totalLoanAmount = totalLoanAmount;
+	}
+
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+
+	public Long getTenure() {
+		return tenure;
+	}
+
+
+	public void setTenure(Long tenure) {
+		this.tenure = tenure;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public byte[] getPropertyImage() {
+		return propertyImage;
+	}
+
+
+	public void setPropertyImage(byte[] propertyImage) {
+		this.propertyImage = propertyImage;
+	}
+
+
+	public SavingsAccount getSavingsAccount() {
+		return savingsAccount;
+	}
+
+
+	public void setSavingsAccount(SavingsAccount savingsAccount) {
+		this.savingsAccount = savingsAccount;
+	}
+
+
+	public List<LoanRepayment> getLoanRepayment() {
+		return loanRepayment;
+	}
+
+
+	public void setLoanRepayment(List<LoanRepayment> loanRepayment) {
+		this.loanRepayment = loanRepayment;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Loan [loanId=" + loanId + ", totalLoanAmount=" + totalLoanAmount + ", interestRate=" + interestRate
+				+ ", tenure=" + tenure + ", status=" + status + ", propertyImage=" + Arrays.toString(propertyImage)
+				+ ", savingsAccount=" + savingsAccount + ", loanRepayment=" + loanRepayment + "]";
+	}
+
+	
+}
---
+++ src/main/java/com/barclays/homeloans/model/LoanRepayment.java
@@ -0,0 +1,142 @@
+package com.barclays.homeloans.model;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+
+
+@Entity
+public class LoanRepayment {
+	
+	@Column
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long loanRepaymentId;
+	
+	
+	@Column
+	@NotNull(message = "EMI cannot be Null")
+	private double emi;
+	
+	
+	@Column
+	@NotNull(message = "Interest Amount cannot be Null")
+	private double interestAmount;
+	
+	@Column
+	@NotNull(message = "Principal Amount cannot be Null")
+	private double principalAmount;
+	
+	@Column
+	@NotNull(message = "Status cannot be Null")
+	private String status;
+	
+	@Column
+	@NotNull(message = "Outstanding cannot be Null")
+	private double outstanding;
+	
+	@Column
+	@NotNull(message = "Date cannot be Null")
+	private Date date;
+	
+
+	
+
+	public LoanRepayment(@NotNull(message = "EMI cannot be Null") double emi,
+			@NotNull(message = "Interest Amount cannot be Null") double interestAmount,
+			@NotNull(message = "Principal Amount cannot be Null") double principalAmount,
+			@NotNull(message = "Status cannot be Null") String status,
+			@NotNull(message = "Outstanding cannot be Null") double outstanding,
+			@NotNull(message = "Date cannot be Null") Date date, Loan loan) {
+		super();
+		this.emi = emi;
+		this.interestAmount = interestAmount;
+		this.principalAmount = principalAmount;
+		this.status = status;
+		this.outstanding = outstanding;
+		this.date = date;
+		
+	}
+
+	public LoanRepayment() {
+		super();
+		// TODO Auto-generated constructor stub
+		
+	}
+
+	public Long getLoanRepaymentId() {
+		return loanRepaymentId;
+	}
+
+	public void setLoanRepaymentId(Long loanRepaymentId) {
+		this.loanRepaymentId = loanRepaymentId;
+	}
+
+	public double getEmi() {
+		return emi;
+	}
+
+	public void setEmi(double emi) {
+		this.emi = emi;
+	}
+
+	public double getInterestAmount() {
+		return interestAmount;
+	}
+
+	public void setInterestAmount(double interestAmount) {
+		this.interestAmount = interestAmount;
+	}
+
+	public double getPrincipalAmount() {
+		return principalAmount;
+	}
+
+	public void setPrincipalAmount(double principalAmount) {
+		this.principalAmount = principalAmount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public double getOutstanding() {
+		return outstanding;
+	}
+
+	public void setOutstanding(double outstanding) {
+		this.outstanding = outstanding;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	@Override
+	public String toString() {
+		return "LoanRepayment [loanRepaymentId=" + loanRepaymentId + ", emi=" + emi + ", interestAmount="
+				+ interestAmount + ", principalAmount=" + principalAmount + ", status=" + status + ", outstanding="
+				+ outstanding + ", date=" + date + "]";
+	}
+
+
+
+
+}
