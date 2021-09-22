6c94927d45f8b7a4e9b88be320a58d345e8867b3
Sanya Varghese <varghese.sanya2gmail.com>
2021-09-22 19:05:12 +0530

    JENKINS TEST

--- src/main/java/com/barclays/homeloans/HomeLoansApplication.java
+++ src/main/java/com/barclays/homeloans/HomeLoansApplication.java
@@ -2,12 +2,14 @@ package com.barclays.homeloans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class HomeLoansApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomeLoansApplication.class, args);
+		
+		
 	}
-
 }
