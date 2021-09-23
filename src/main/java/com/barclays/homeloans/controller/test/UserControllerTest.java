0ff8566f0ecd315a27325b8dce6a3d7aebfa7809
finessebarot <imharshitbarot18@gmail.com>
2021-09-23 09:50:57 +0530

    -Change of Directory please take the pull for  user services

--- src/main/java/com/barclays/homeloans/controller/test/UserControllerTest.java
+++
@@ -1,5 +0,0 @@
-package com.barclays.homeloans.controller.test;
-
-public class UserControllerTest {
-
-}
---
+++ src/test/java/com/barclays/homeloans/controller/test/UserControllerTest.java
@@ -0,0 +1,21 @@
+package com.barclays.homeloans.controller.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;  
+import org.junit.After;  
+import org.junit.AfterClass;  
+import org.junit.Before;  
+import org.junit.BeforeClass;  
+
+public class UserControllerTest {
+	
+//	@Test
+//	public void testRegister() {
+//		String str= "I am done with Junit setup";
+//	      assertEquals("I am done with Junit setup",str);
+//		
+		
+	}
+}
